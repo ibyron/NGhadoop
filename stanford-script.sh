4d9fa3ce0be90299baa1ba57293c88d52fe7b503
root <root@hansible.gr>
2014-05-25 21:16:01 +0300

    stanford-script.sh minor modification

--- stanford-script.sh
+++ stanford-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script to run stanford nlp splitting and tokenization over a filelist
-java -cp stanford-corenlp-3.3.1.jar:stanford-corenlp-3.3.1-models.jar:xom.jar:joda-time.jar:jollyday.jar:ejml-0.23.jar -Xmx4g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit -tokenize.options untokenizable=noneDelete,normalizeParentheses=false,normalizeOtherBrackets =false,latexQuotes=false -filelist filelist
+java -cp stanford-corenlp-3.3.1.jar:stanford-corenlp-3.3.1-models.jar:xom.jar:joda-time.jar:jollyday.jar:ejml-0.23.jar -Xmx4g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit -tokenize.options untokenizable=noneDelete,normalizeParentheses=false,normalizeOtherBrackets=false,latexQuotes=false -filelist filelist
 
 # post-processing, keep only the word tokens from the xml
 for f in ./wiki_*xml
